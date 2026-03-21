d8f2a0b5844fd8548d23a61d690e048d6532a702
OcampoEmiliano <ocampoemilianopa@gmail.com>
2026-03-21 14:58:00 -0300

    categorias agregadas a la base de datos

--- init.sql
+++ init.sql
@@ -2,14 +2,14 @@
 -- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3307
--- Tiempo de generación: 14-03-2026 a las 23:09:06
--- Versión del servidor: 11.4.9-MariaDB
--- Versión de PHP: 8.3.28
+-- Servidor: db
+-- Tiempo de generación: 21-03-2026 a las 17:49:58
+-- Versión del servidor: 11.4.10-MariaDB-ubu2404
+-- Versión de PHP: 8.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "-03:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,14 +27,24 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `categorias`
 --
 
-DROP TABLE IF EXISTS `categorias`;
-CREATE TABLE IF NOT EXISTS `categorias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `slug` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `slug` (`slug`)
+  `slug` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `slug`) VALUES
+(1, 'Actualidad', 'actualidad'),
+(2, 'Deportes', 'deportes'),
+(3, 'Tecnología', 'tecnologia'),
+(4, 'Cultura', 'cultura'),
+(5, 'Economía', 'economia'),
+(6, 'Espectáculos', 'espectaculos'),
+(7, 'Salud', 'salud');
 
 -- --------------------------------------------------------
 
@@ -42,9 +52,8 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 -- Estructura de tabla para la tabla `noticias`
 --
 
-DROP TABLE IF EXISTS `noticias`;
-CREATE TABLE IF NOT EXISTS `noticias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
   `slug` varchar(100) NOT NULL,
   `copete` text NOT NULL,
@@ -53,10 +62,7 @@ CREATE TABLE IF NOT EXISTS `noticias` (
   `fecha_publicacion` timestamp NULL DEFAULT current_timestamp(),
   `categoria_id` int(11) NOT NULL,
   `autor` varchar(100) NOT NULL,
-  `estado` enum('borrador','publicado') NOT NULL DEFAULT 'borrador',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `slug_unico` (`slug`),
-  KEY `fk_categoria` (`categoria_id`)
+  `estado` enum('borrador','publicado') NOT NULL DEFAULT 'borrador'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -65,16 +71,61 @@ CREATE TABLE IF NOT EXISTS `noticias` (
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `rol` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  `rol` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug_unico` (`slug`),
+  ADD KEY `fk_categoria` (`categoria_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
